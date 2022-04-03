d5a9fd20d6a53b023a3306ae608334a56c1b3abe
Samuel Martín Morales <72341631+Samuelmm15@users.noreply.github.com>
2022-04-03 10:22:00 +0100

    Advanced

--- Introduccion-JavaScript/js/insert.js
+++ Introduccion-JavaScript/js/insert.js
@@ -1,7 +1,17 @@
-const insertedText = document.querySelector('.insertedText');
-const buttonText = document.querySelector('.button');
-const recomendation = document.querySelector('.Recomendation');
-function addRecomendation() {
-  recomendation.textContent = insertedText.value;
-}
-buttonText.addEventListener('click', addRecomendation);
+(function($){
+  $(function(){
+
+    $('.sidenav').sidenav();
+    $('.parallax').parallax();
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
+
+document.addEventListener('DOMContentLoaded', function() {
+  const buttonText = document.getElementById("button");
+  function addRecomendation() {
+    const insertedText = document.getElementById("insertedText").value;
+    document.getElementById("Recomendation").innerHTML = ' '+insertedText;
+  }
+  buttonText.onclick = addRecomendation;
+});
