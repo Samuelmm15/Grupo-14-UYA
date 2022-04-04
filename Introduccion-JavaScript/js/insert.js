c9a01563a692438f892e92c49a3528971fb006a1
Alejandro Lugo Fumero <64960467+AKALugo@users.noreply.github.com>
2022-04-04 19:56:11 +0100

    Add files via upload

---
+++ Introduccion-JavaScript/js/insert.js
@@ -0,0 +1,19 @@
+(function($){
+  $(function(){
+
+    $('.sidenav').sidenav();
+    $('.parallax').parallax();
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
+
+document.addEventListener('DOMContentLoaded', function() {
+  const buttonText = document.getElementById("button");
+  function addRecomendation() {
+    const insertedText = document.getElementById("insertedText").value;
+    document.getElementById("Recomendation").innerHTML = ' '+insertedText;
+  }
+  buttonText.onclick = addRecomendation;
+});
+
+
