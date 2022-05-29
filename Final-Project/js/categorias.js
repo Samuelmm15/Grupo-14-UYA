d1b98aa390e43b6358ead2816e846aa9c7972179
Samuel Martín Morales <72341631+Samuelmm15@users.noreply.github.com>
2022-05-29 12:53:37 +0100

    Added the frecuently questions page

---
+++ Final-Project/js/categorias.js
@@ -0,0 +1,22 @@
+const categorias = document.querySelectorAll('#categorias .categoria');
+const contenedorPreguntas = document.querySelectorAll('.contenedor-preguntas');
+let categoriaActiva = null;
+
+categorias.forEach((categoria) => {
+	categoria.addEventListener('click', (e) => {
+		categorias.forEach((elemento) => {
+			elemento.classList.remove('activa');
+		});
+
+		e.currentTarget.classList.toggle('activa');
+		categoriaActiva = categoria.dataset.categoria;
+
+		contenedorPreguntas.forEach((contenedor) => {
+			if(contenedor.dataset.categoria === categoriaActiva){
+				contenedor.classList.add('activo');
+			} else {
+				contenedor.classList.remove('activo');
+			}
+		});
+	});
+});
